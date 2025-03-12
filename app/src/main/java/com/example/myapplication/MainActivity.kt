6a3c146b1f4bfadb48bcfa176bfb7777e253b929
srivas-ITB <santiago.rivas@itb.cat>
2025-03-12 21:47:40 +0100

    Ex1,2 i 4 fets. Ex3, a mitges

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,15 +21,21 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
@@ -36,11 +45,11 @@ import com.example.myapplication.ui.theme.MyApplicationTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
+        enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { padding ->
-                    MyConstraintLayout( modifier = Modifier.padding(padding), 4)
+                    ExercicisStateless(modifier = Modifier.padding(padding), 1)
                 }
 
             }
@@ -49,91 +58,212 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Alerta(exercici: Int, modifier: Modifier = Modifier) {
     Text(
-        text = "Hola $name!",
+        text = "ERROR l'exercici $exercici no existeix.",
         modifier = modifier
     )
 }
 
 @Composable
-fun MyConstraintLayout( modifier: Modifier = Modifier, tipus:Int = 1) {
+fun ExercicisStateless(modifier: Modifier = Modifier, exercici: Int = 1) {
 
-    when (tipus) {
-        1 -> MyConstraintLayout_withGuidelines(modifier)
-        2 -> MyConstraintLayout_withoutGuidelines(modifier)
-        3 -> MyConstraintLayout_chains(modifier)
-        4 -> MyConstraintLayout_text(modifier)
+    when (exercici) {
+        1 -> Exercici1(modifier)
+        2 -> Exercici2(modifier)
+        3 -> Exercici3(modifier)
+        4 -> Exercici4(modifier)
         5 -> MyConstraintLayout_Images(modifier)
-        else -> Greeting("ERROR", modifier)
+        else -> Alerta(exercici, modifier)
+    }
+}
+
+@Composable
+fun Exercici1(modifier: Modifier = Modifier) {
+    ConstraintLayout(modifier
+        .fillMaxSize()
+        .padding(vertical = 50.dp)) {
+        Column(modifier.fillMaxSize()) {
+            Image(
+                painter = painterResource(id = R.drawable.bg_compose_background),
+                contentDescription = "Example"
+            )
+            Text(
+                stringResource(R.string.ex1_title),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(10.dp)
+            )
+            Text(stringResource(R.string.ex1_par1), modifier = modifier.padding(10.dp), textAlign = TextAlign.Justify)
+            Text(stringResource(R.string.ex1_par2), modifier = modifier.padding(10.dp), textAlign = TextAlign.Justify)
+        }
+    }
+}
+
+@Composable
+fun Exercici2(modifier: Modifier = Modifier) {
+    ConstraintLayout(modifier
+        .fillMaxSize()
+        .padding(vertical = 50.dp)) {
+        Column(modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_task_completed),
+                contentDescription = "Completed",
+                modifier = modifier.align(Alignment.CenterHorizontally)
+            )
+            Text(stringResource(R.string.ex2_allTasksCompleted),
+                modifier = modifier.padding(10.dp).align(Alignment.CenterHorizontally),
+                fontWeight =  FontWeight.Bold
+            )
+            Text(stringResource(R.string.ex2_niceWork),
+                modifier = modifier.align(Alignment.CenterHorizontally))
+        }
+    }
+}
+
+@Composable
+fun Exercici3(modifier: Modifier = Modifier) {
+    Column() {
+        Row() {
+            Column() {
+                Text(
+                    stringResource(R.string.ex3_box1_title),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(10.dp).weight(1.0f)
+                )
+                Text(
+                    stringResource(R.string.ex3_box1_text),
+                    modifier = modifier.padding(10.dp).weight(1.0f),
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column() {
+                Text(
+                    stringResource(R.string.ex3_box2_title),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(10.dp).weight(1.0f)
+                )
+                Text(
+                    stringResource(R.string.ex3_box2_text),
+                    modifier = modifier.padding(10.dp).weight(1.0f),
+                    textAlign = TextAlign.Justify
+                )
+
+            }
+        }
+
+        Row( ) {
+            Column() {
+                Text(
+                    stringResource(R.string.ex3_box3_title),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(10.dp).weight(1.0f)
+                )
+                Text(
+                    stringResource(R.string.ex3_box3_text),
+                    modifier = modifier.padding(10.dp).weight(1.0f),
+                    textAlign = TextAlign.Justify
+                )
+            }
+            Column() {
+                Text(
+                    stringResource(R.string.ex3_box4_title),
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(10.dp).weight(1.0f)
+                )
+                Text(
+                    stringResource(R.string.ex3_box4_text),
+                    modifier = modifier.padding(10.dp).weight(1.0f),
+                    textAlign = TextAlign.Justify
+                )
+
+            }
+        }
+
     }
 }
 
 
 @Composable
-fun MyConstraintLayout_withoutGuidelines(modifier: Modifier = Modifier) {
+fun Exercici4(modifier: Modifier = Modifier) {
     ConstraintLayout(modifier.fillMaxSize()) {
         val (boxRed, boxYellow, boxGreen, boxCyan, boxMagenta) = createRefs()
 
-        Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed) {
-            top.linkTo(parent.top)
-            bottom.linkTo(parent.bottom)
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        })
-        Box(modifier = Modifier.size(120.dp).background(Color.Yellow).constrainAs(boxYellow) {
-            bottom.linkTo(boxRed.top)
-            start.linkTo(boxRed.start)
-        })
-        Box(modifier = Modifier.size(120.dp).background(Color.Green).constrainAs(boxGreen) {
-            top.linkTo(boxRed.bottom)
-            start.linkTo(boxRed.start)
-        })
-        Box(modifier = Modifier.size(120.dp).background(Color.Cyan).constrainAs(boxCyan) {
-            bottom.linkTo(boxRed.bottom)
-            end.linkTo(boxRed.start)
-        })
-        Box(modifier = Modifier.size(120.dp).background(Color.Magenta).constrainAs(boxMagenta) {
-            bottom.linkTo(boxRed.bottom)
-            start.linkTo(boxRed.end)
-        })
+        Box(modifier = Modifier
+            .size(120.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                top.linkTo(parent.top)
+                bottom.linkTo(parent.bottom)
+                start.linkTo(parent.start)
+                end.linkTo(parent.end)
+            })
+        Box(modifier = Modifier
+            .size(120.dp)
+            .background(Color.Yellow)
+            .constrainAs(boxYellow) {
+                bottom.linkTo(boxRed.top)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(120.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                top.linkTo(boxRed.bottom)
+                start.linkTo(boxRed.end)
+            })
+        Box(modifier = Modifier
+            .size(120.dp)
+            .background(Color.Cyan)
+            .constrainAs(boxCyan) {
+                top.linkTo(boxRed.bottom)
+                end.linkTo(boxRed.start)
+            })
+        Box(modifier = Modifier
+            .size(120.dp)
+            .background(Color.Magenta)
+            .constrainAs(boxMagenta) {
+                bottom.linkTo(boxRed.top)
+                start.linkTo(boxRed.end)
+            })
 
     }
 }
 
-@Composable
-fun MyConstraintLayout_withGuidelines(modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier.fillMaxSize()) {
-        val topGuide = createGuidelineFromTop(0.25f)
-        val leftGuide = createGuidelineFromStart(0.25f)
-        //val bottomGuide = createGuidelineFromBottom(0.25f)
-        //val rightGuide = createGuidelineFromEnd(0.25f)
 
-        val boxRed = createRef()
-        Box(modifier = Modifier.size(120.dp).background(Color.Red).constrainAs(boxRed){
-            top.linkTo(topGuide)
-            start.linkTo(leftGuide)
-        })
-
-    }
-}
 
 @Composable
 fun MyConstraintLayout_chains(modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier.fillMaxSize().padding(vertical=100.dp)) {
+    ConstraintLayout(modifier
+        .fillMaxSize()
+        .padding(vertical = 100.dp)) {
         val (boxRed, boxYellow, boxGreen) = createRefs()
-        Box(modifier.size(50.dp).background(Color.Red).constrainAs(boxRed){
-            start.linkTo(parent.start)
-            end.linkTo(boxYellow.start)
-        })
-        Box(modifier.size(50.dp).background(Color.Yellow).constrainAs(boxYellow){
-            start.linkTo(boxRed.end)
-            end.linkTo(boxGreen.start)
-        })
-        Box(modifier.size(50.dp).background(Color.Green).constrainAs(boxGreen){
-            start.linkTo(boxYellow.end)
-            end.linkTo(parent.end)
-        })
+        Box(modifier
+            .size(50.dp)
+            .background(Color.Red)
+            .constrainAs(boxRed) {
+                start.linkTo(parent.start)
+                end.linkTo(boxYellow.start)
+            })
+        Box(modifier
+            .size(50.dp)
+            .background(Color.Yellow)
+            .constrainAs(boxYellow) {
+                start.linkTo(boxRed.end)
+                end.linkTo(boxGreen.start)
+            })
+        Box(modifier
+            .size(50.dp)
+            .background(Color.Green)
+            .constrainAs(boxGreen) {
+                start.linkTo(boxYellow.end)
+                end.linkTo(parent.end)
+            })
 
         //createHorizontalChain(boxRed, boxYellow, boxGreen, chainStyle = ChainStyle.Packed)
         //createHorizontalChain(boxRed, boxYellow, boxGreen, chainStyle = ChainStyle.Spread)
@@ -143,7 +273,9 @@ fun MyConstraintLayout_chains(modifier: Modifier = Modifier) {
 
 @Composable
 fun MyConstraintLayout_text(modifier: Modifier = Modifier) {
-    Column(modifier.fillMaxSize().padding(vertical=100.dp)) {
+    Column(modifier
+        .fillMaxSize()
+        .padding(vertical = 100.dp)) {
         Text(text = "This is my text example")
         Text(
             "This is my text example",
@@ -154,11 +286,13 @@ fun MyConstraintLayout_text(modifier: Modifier = Modifier) {
             textDecoration = TextDecoration.Underline
         )
 
-        val myStyle = TextStyle(color = Color.Red,
+        val myStyle = TextStyle(
+            color = Color.Red,
             fontSize = 50.sp,
             fontWeight = FontWeight.Bold,
             fontFamily = FontFamily.Cursive,
-            textDecoration = TextDecoration.Underline)
+            textDecoration = TextDecoration.Underline
+        )
 
         Text(
             "This is my text example",
@@ -171,7 +305,9 @@ fun MyConstraintLayout_text(modifier: Modifier = Modifier) {
 
 @Composable
 fun MyConstraintLayout_Images(modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier.fillMaxSize().padding(vertical=100.dp)) {
+    ConstraintLayout(modifier
+        .fillMaxSize()
+        .padding(vertical = 100.dp)) {
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = "Example",
@@ -179,16 +315,20 @@ fun MyConstraintLayout_Images(modifier: Modifier = Modifier) {
             //modifier = Modifier.clip(RoundedCornerShape(50f))
             //modifier = Modifier.clip(CircleShape)
             //modifier = Modifier.clip(CircleShape).border(4.dp, Color.Red)
-            modifier = Modifier.clip(CircleShape).border(4.dp, Color.Red, CircleShape)
+            modifier = Modifier
+                .clip(CircleShape)
+                .border(4.dp, Color.Red, CircleShape)
         )
         Image(
             painter = painterResource(id = R.drawable.ic_launcher_foreground),
             contentDescription = "Example",
             alpha = 1f
         )
-        Icon(imageVector = Icons.Sharp.Star,
+        Icon(
+            imageVector = Icons.Sharp.Star,
             contentDescription = "Icon Example",
-            tint = Color.Blue)
+            tint = Color.Blue
+        )
     }
 
 }
@@ -198,6 +338,6 @@ fun MyConstraintLayout_Images(modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        MyConstraintLayout(tipus = 5)
+        ExercicisStateless(exercici = 3)
     }
 }
