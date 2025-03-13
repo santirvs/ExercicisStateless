2cb450eca64979f683f68b778d8b0a691817cc24
srivas-ITB <santiago.rivas@itb.cat>
2025-03-13 21:32:57 +0100

    Fets de l'1 al 4. Fent en 5

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.sharp.Star
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
@@ -25,21 +26,20 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.HorizontalAlign
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -73,7 +73,7 @@ fun ExercicisStateless(modifier: Modifier = Modifier, exercici: Int = 1) {
         2 -> Exercici2(modifier)
         3 -> Exercici3(modifier)
         4 -> Exercici4(modifier)
-        5 -> MyConstraintLayout_Images(modifier)
+        5 -> Exercici5(modifier)
         else -> Alerta(exercici, modifier)
     }
 }
@@ -94,8 +94,15 @@ fun Exercici1(modifier: Modifier = Modifier) {
                 fontWeight = FontWeight.Bold,
                 modifier = modifier.padding(10.dp)
             )
-            Text(stringResource(R.string.ex1_par1), modifier = modifier.padding(10.dp), textAlign = TextAlign.Justify)
-            Text(stringResource(R.string.ex1_par2), modifier = modifier.padding(10.dp), textAlign = TextAlign.Justify)
+            Text(
+                stringResource(R.string.ex1_par1),
+                modifier = modifier.padding(10.dp),
+                textAlign = TextAlign.Justify
+            )
+            Text(stringResource(R.string.ex1_par2),
+                modifier = modifier.padding(10.dp),
+                textAlign = TextAlign.Justify
+            )
         }
     }
 }
@@ -125,60 +132,79 @@ fun Exercici2(modifier: Modifier = Modifier) {
 @Composable
 fun Exercici3(modifier: Modifier = Modifier) {
     Column() {
-        Row() {
-            Column() {
+        Row(modifier.weight(1.0f)) {
+            Column(modifier.weight(1.0f)
+                    .background(color=colorResource(R.color.ex4_color1))
+                    .fillMaxSize( ),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
                     stringResource(R.string.ex3_box1_title),
-                    fontSize = 20.sp,
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = modifier.padding(10.dp).weight(1.0f)
+                    modifier = modifier.padding(10.dp)
                 )
                 Text(
                     stringResource(R.string.ex3_box1_text),
-                    modifier = modifier.padding(10.dp).weight(1.0f),
+                    modifier = modifier.padding(10.dp),
                     textAlign = TextAlign.Justify
                 )
             }
-            Column() {
+            Column(modifier.weight(1.0f)
+                    .background(color=colorResource(R.color.ex4_color2))
+                    .fillMaxSize( ),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
                     stringResource(R.string.ex3_box2_title),
-                    fontSize = 20.sp,
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = modifier.padding(10.dp).weight(1.0f)
+                    modifier = modifier.padding(10.dp)
                 )
                 Text(
                     stringResource(R.string.ex3_box2_text),
-                    modifier = modifier.padding(10.dp).weight(1.0f),
+                    modifier = modifier.padding(10.dp),
                     textAlign = TextAlign.Justify
                 )
 
             }
         }
 
-        Row( ) {
-            Column() {
+        Row( modifier.weight(1.0f) ) {
+            Column(modifier.weight(1.0f)
+                    .background(color=colorResource(R.color.ex4_color3))
+                    .fillMaxSize( ),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally) {
                 Text(
                     stringResource(R.string.ex3_box3_title),
-                    fontSize = 20.sp,
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = modifier.padding(10.dp).weight(1.0f)
+                    modifier = modifier.padding(10.dp),
+
                 )
                 Text(
                     stringResource(R.string.ex3_box3_text),
-                    modifier = modifier.padding(10.dp).weight(1.0f),
+                    modifier = modifier.padding(10.dp),
                     textAlign = TextAlign.Justify
                 )
             }
-            Column() {
+            Column(modifier.weight(1.0f)
+                    .background(color=colorResource(R.color.ex4_color4))
+                    .fillMaxSize( ),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 Text(
                     stringResource(R.string.ex3_box4_title),
-                    fontSize = 20.sp,
+                    fontSize = 16.sp,
                     fontWeight = FontWeight.Bold,
-                    modifier = modifier.padding(10.dp).weight(1.0f)
+                    modifier = modifier.padding(10.dp)
+
                 )
                 Text(
                     stringResource(R.string.ex3_box4_text),
-                    modifier = modifier.padding(10.dp).weight(1.0f),
+                    modifier = modifier.padding(10.dp),
                     textAlign = TextAlign.Justify
                 )
 
@@ -187,7 +213,6 @@ fun Exercici3(modifier: Modifier = Modifier) {
 
     }
 }
-
 
 @Composable
 fun Exercici4(modifier: Modifier = Modifier) {
@@ -235,109 +260,87 @@ fun Exercici4(modifier: Modifier = Modifier) {
     }
 }
 
-
-
 @Composable
-fun MyConstraintLayout_chains(modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier
-        .fillMaxSize()
-        .padding(vertical = 100.dp)) {
-        val (boxRed, boxYellow, boxGreen) = createRefs()
-        Box(modifier
-            .size(50.dp)
-            .background(Color.Red)
-            .constrainAs(boxRed) {
-                start.linkTo(parent.start)
-                end.linkTo(boxYellow.start)
-            })
-        Box(modifier
-            .size(50.dp)
-            .background(Color.Yellow)
-            .constrainAs(boxYellow) {
-                start.linkTo(boxRed.end)
-                end.linkTo(boxGreen.start)
-            })
-        Box(modifier
-            .size(50.dp)
-            .background(Color.Green)
-            .constrainAs(boxGreen) {
-                start.linkTo(boxYellow.end)
-                end.linkTo(parent.end)
-            })
-
-        //createHorizontalChain(boxRed, boxYellow, boxGreen, chainStyle = ChainStyle.Packed)
-        //createHorizontalChain(boxRed, boxYellow, boxGreen, chainStyle = ChainStyle.Spread)
-        createHorizontalChain(boxRed, boxYellow, boxGreen, chainStyle = ChainStyle.SpreadInside)
-    }
-}
-
-@Composable
-fun MyConstraintLayout_text(modifier: Modifier = Modifier) {
-    Column(modifier
-        .fillMaxSize()
-        .padding(vertical = 100.dp)) {
-        Text(text = "This is my text example")
-        Text(
-            "This is my text example",
-            color = Color.Red,
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.Cursive,
-            textDecoration = TextDecoration.Underline
-        )
-
-        val myStyle = TextStyle(
-            color = Color.Red,
-            fontSize = 50.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = FontFamily.Cursive,
-            textDecoration = TextDecoration.Underline
-        )
-
-        Text(
-            "This is my text example",
-            style = myStyle
-        )
-
-
-    }
-}
-
-@Composable
-fun MyConstraintLayout_Images(modifier: Modifier = Modifier) {
-    ConstraintLayout(modifier
-        .fillMaxSize()
-        .padding(vertical = 100.dp)) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Example",
-            alpha = 0.75f,
-            //modifier = Modifier.clip(RoundedCornerShape(50f))
-            //modifier = Modifier.clip(CircleShape)
-            //modifier = Modifier.clip(CircleShape).border(4.dp, Color.Red)
-            modifier = Modifier
-                .clip(CircleShape)
-                .border(4.dp, Color.Red, CircleShape)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-            contentDescription = "Example",
-            alpha = 1f
-        )
-        Icon(
-            imageVector = Icons.Sharp.Star,
-            contentDescription = "Icon Example",
-            tint = Color.Blue
-        )
+fun Exercici5(modifier: Modifier = Modifier) {
+    Column(modifier.fillMaxSize()
+        //.background(color=colorResource(R.color.ex5_background))
+        .background(color=Color.Yellow)
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment =  Alignment.CenterVertically,
+            modifier = modifier.fillMaxSize().weight(0.25f)) {
+            Box(
+                modifier
+                    .size(100.dp)
+                    .background(Color.Red)
+            )
+        }
+        Row( modifier = modifier.fillMaxSize().weight(0.1f),
+            horizontalArrangement = Arrangement.Center) {
+            Text(
+                "Jeniffer Doe",
+                fontSize = 40.sp,
+                modifier = modifier.padding(10.dp)
+            )
+        }
+        Row( modifier = modifier.fillMaxSize().weight(0.1f),
+            horizontalArrangement = Arrangement.Center) {
+            Text(
+                "Android Developer Extraordinaire",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(10.dp)
+            )
+        }
+        Row(  modifier = modifier.fillMaxSize().weight(0.1f),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(imageVector = Icons.Default.Star,
+                contentDescription = "Icon Example",
+                tint = Color.Blue)
+            Text(
+                "+11 (123) 444 555 666",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(10.dp)
+            )
+        }
+        Row(  modifier = modifier.fillMaxSize().weight(0.1f),
+            horizontalArrangement = Arrangement.Center) {
+            Icon(imageVector = Icons.Default.Star,
+                contentDescription = "Icon Example",
+                tint = Color.Blue)
+            Text(
+                "@AndroidDev",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(10.dp)
+            )
+        }
+        Row(  modifier = modifier.fillMaxSize().weight(0.1f),
+            horizontalArrangement = Arrangement.Center) {
+            Icon(imageVector = Icons.Default.Star,
+                contentDescription = "Icon Example",
+                tint = Color.Blue
+            )
+            Text(
+                "jen.doe@android.com",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.padding(10.dp)
+            )
+        }
     }
 
 }
+
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        ExercicisStateless(exercici = 3)
+        ExercicisStateless(exercici = 5)
     }
 }
